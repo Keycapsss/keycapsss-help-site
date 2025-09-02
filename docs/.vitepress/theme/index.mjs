ae9d1bf942a8294f9b136ccd6f19bf3de03c2978
Ben Roe <>
2025-09-02 12:14:49 +0200

    feat: add embed youtube videos in markdown

--- docs/.vitepress/theme/index.mjs
+++ docs/.vitepress/theme/index.mjs
@@ -1,4 +1,13 @@
 import DefaultTheme from "vitepress/theme";
+
+import { YouTubeEmbed } from "@miletorix/vitepress-youtube-embed"; // [!code ++]
+import "@miletorix/vitepress-youtube-embed/style.css"; // [!code ++]
+
 import "./custom.css";
 
-export default DefaultTheme;
+export default {
+  extends: DefaultTheme,
+  enhanceApp(ctx) {
+    ctx.app.component("YouTubeEmbed", YouTubeEmbed); // [!code ++]
+  },
+};
